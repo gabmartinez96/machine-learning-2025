b7717f9363dd2e5fcced1fa8b31ba867b92f7d81
Gab Martinez <gabrieltmartinez96@gmail.com>
2026-06-30 13:43:33 -0300

    feature: add .gitignore and update cerveja.py

--- cerveja.py
+++ cerveja.py
@@ -9,6 +9,7 @@ df.head()
 # %%
 
 from sklearn import linear_model
+from sklearn import tree
 
 # Separou as variáveis independentes e a resposta
 X = df[['cerveja']] # O X vai ser uma matriz (Dataframe)
@@ -18,17 +19,24 @@ y = df['nota'] # A variavel resposta vai ser um vetor (Serie)
 reg = linear_model.LinearRegression()
 reg.fit(X, y)
 
-#%%
-
 # Visualizar os coeficientes da regressão
 a, b = reg.intercept_, reg.coef_[0]
-print(a,b)
-
-#%%
 
 # Testar o modelo para predizer dados
-predict =  reg.predict(X.drop_duplicates())
+predict_reg =  reg.predict(X.drop_duplicates())
 
+# Criar um modelo de regressão em arvore em Full Depth
+arvore_full = tree.DecisionTreeRegressor(random_state=42)
+arvore_full.fit(X, y)
+predict_arvore_full = arvore_full.predict(X.drop_duplicates())
+
+# Criar um modelo de regressão em arvore depth = 2
+arvore_d2 = tree.DecisionTreeRegressor(random_state=42,
+                                       max_depth=2)
+arvore_d2.fit(X, y)
+predict_arvore_d2 = arvore_d2.predict(X.drop_duplicates())
+
+#%%
 import matplotlib.pyplot as plt
 
 plt.plot(X['cerveja'], y, 'o')
@@ -37,6 +45,15 @@ plt.title("Relação Cerveja vs Nota")
 plt.xlabel('Cerveja')
 plt.ylabel('Nota')
 
-plt.plot(X['cerveja'].drop_duplicates(), predict)
+plt.plot(X['cerveja'].drop_duplicates(), predict_reg)
+plt.plot(X['cerveja'].drop_duplicates(), predict_arvore_full, color='green')
+plt.plot(X['cerveja'].drop_duplicates(), predict_arvore_d2, color='magenta')
 
-plt.legend(['Observado', f'y = {a:.3f} + {b:.3f}x'])
+plt.legend(['Observado', f'y = {a:.3f} + {b:.3f}x', 'Arvore Full',
+            'Arvore Depth = 2'])
+
+# %%
+plt.figure(dpi=400)
+tree.plot_tree(arvore_d2,
+               feature_names=['cerveja'],
+               filled=True)
