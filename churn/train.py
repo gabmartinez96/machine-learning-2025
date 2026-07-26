1ee4543a0b6825670c51865c74fabdb72e83dc0a
Gab Martinez <gabrieltmartinez96@gmail.com>
2026-07-26 17:54:43 -0300

    "feat: criando assess"

--- churn/train.py
+++ churn/train.py
@@ -58,14 +58,15 @@ sumario.sort_values(by=['diff_rel'], ascending=False)
 
 # %%
 
-from sklearn import tree
 import matplotlib.pyplot as plt
+from feature_engine import discretisation, encoding # type: ignore
+from sklearn import pipeline, linear_model, metrics, naive_bayes, tree, ensemble
 
 arvore = tree.DecisionTreeClassifier(random_state=42, max_depth=5)
 arvore.fit(X_train, y_train)
 
 # %%
-
+# MODIFY
 # Definindo as principais features
 feature_importance = (pd.Series(arvore.feature_importances_,
                                 index=X_train.columns)
@@ -74,15 +75,6 @@ feature_importance = (pd.Series(arvore.feature_importances_,
 feature_importance['acum'] = feature_importance[0].cumsum()
 best_features = (feature_importance[feature_importance['acum'] < 0.96]['index']
                  .tolist())
-best_features
-
-# %%
-
-# MODIFY
-from feature_engine import discretisation, encoding # type: ignore
-from sklearn import pipeline, linear_model, metrics
-
-
 ## Discretizar
 # Regression = False se as variáveis forem para modelar uma classificação
 tree_discretization = discretisation.DecisionTreeDiscretiser(variables=best_features,
@@ -94,15 +86,25 @@ tree_discretization = discretisation.DecisionTreeDiscretiser(variables=best_feat
 # OneHot
 onehot = encoding.OneHotEncoder(variables=best_features, ignore_format=True)
 
+# %%
+
 # Model
-reg = linear_model.LogisticRegression(penalty=None, random_state=42, max_iter=10000)
+# model = linear_model.LogisticRegression(penalty=None, random_state=42, max_iter=10000)
+# model = naive_bayes.BernoulliNB()
+model = ensemble.RandomForestClassifier(random_state=42,
+                                        min_samples_leaf=20,
+                                        n_jobs=-1,
+                                        n_estimators=1000
+                                
+
+)
 
 # Model pipeline
 model_pipeline = pipeline.Pipeline(
     steps = [
         ("Discretizar", tree_discretization),
         ("OneHot", onehot),
-        ("Model", reg)
+        ("Model", model)
     ]
 )
 
@@ -116,6 +118,7 @@ y_train_proba = model_pipeline.predict_proba(X_train)[:,1]
 # Calcular as métricas na base de dados de treino
 acc_train = metrics.accuracy_score(y_train, y_train_predict)
 auc_train = metrics.roc_auc_score(y_train, y_train_proba)
+roc_train = metrics.roc_curve(y_train,y_train_proba)
 print(f"Acurácia Treino: {acc_train}")
 print(f"AUC Treino: {auc_train}")
 
@@ -127,6 +130,7 @@ y_test_proba = model_pipeline.predict_proba(X_test)[:,1]
 # Calcular as métricas na base de dados de test
 acc_test = metrics.accuracy_score(y_test, y_test_predict)
 auc_test = metrics.roc_auc_score(y_test, y_test_proba)
+roc_test = metrics.roc_curve(y_test,y_test_proba)
 print(f"Acurácia teste: {acc_test}")
 print(f"AUC teste: {auc_test}")
 
@@ -139,6 +143,16 @@ oot_proba = model_pipeline.predict_proba(oot[features])[:,1]
 # Calcular as métricas na base de dados de test
 acc_oot = metrics.accuracy_score(oot[target], oot_predict)
 auc_oot = metrics.roc_auc_score(oot[target], oot_proba)
+roc_oot = metrics.roc_curve(oot[target],oot_proba)
 print(f"Acurácia oot: {acc_oot}")
 print(f"AUC oot: {auc_oot}")
+# %%
+
+# Plotar curvas roc
+plt.plot(roc_train[0], roc_train[1], label=f'Treino: {auc_train*100:.2f}')
+plt.plot(roc_test[0], roc_test[1], label=f'Teste: {auc_test*100:.2f}')
+plt.plot(roc_oot[0], roc_oot[1], label=f'Out-of-Time: {auc_oot*100:.2f}')
+plt.grid()
+plt.legend()
+plt.title('Curva ROC')
 # %%
